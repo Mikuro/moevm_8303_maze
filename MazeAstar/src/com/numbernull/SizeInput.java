d18c1cada878c60b31b5214ec89adadea1c53418
Artem.Danilov <42946292+Pamr0ck@users.noreply.github.com>
2020-07-06 05:34:28 +0300

    it remains to delete the field X

--- MazeAstar/src/com/numbernull/SizeInput.java
+++ MazeAstar/src/com/numbernull/SizeInput.java
@@ -22,7 +22,7 @@ public class SizeInput extends JFrame{
         super(s);
         setLayout(new GridLayout(3, 1, 10, 10));
         this.ok = new JButton("Ok");
-        this.info = new JLabel("Введите координаты:");
+        this.info = new JLabel("Введите размер:");
         this.xCord = new JTextField("ширина лабиринта", 5);
         this.yCord = new JTextField("длина лабиринта", 5);
         add(this.info);
@@ -37,12 +37,12 @@ public class SizeInput extends JFrame{
             public void actionPerformed(ActionEvent e) {
                 if(e.getSource() == ok){
                     try {
-                        x = Integer.parseInt(xCord.getText());
+//                        x = Integer.parseInt(xCord.getText());
                         y = Integer.parseInt(yCord.getText());
                         setVisible(false);
-                        if (x < 4 || x > 150 || y < 4 || y > 150)
+                        if (y < 4 || y > 150)
                             throw new RangeException((short) 0,"size should be from 4 to 150");
-                        new MazeWindow(s, x, y, new Maze(x, y));
+                        new MazeWindow(s, y, y, new Maze(y, y));
                     }catch (NumberFormatException err){
                         System.out.println("nice try");
                         //TODO кинуть окно, мол введи норм
